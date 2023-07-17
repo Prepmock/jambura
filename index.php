927225b7bee3ae43019a666bcd1c506210e41c74
Shahriar <shahriar.hossain@prepmock.com>
2023-07-17 12:28:02 +0600

    feature/JAM-15 : changed index.php to require the new jambura file

--- index.php
+++ index.php
@@ -4,11 +4,18 @@
 # database access info and all.
 include('configurations.php');
 include('autoload.php');
+
+#including mvc files
+define('JAMBURA_MVC', 'vendor/prepmock/jambura-core/src/mvc');
+foreach (glob(JAMBURA_MVC . '/*' . EXT) as $filename) {
+    include $filename;
+}
+
 # Register autoloader
 spl_autoload_register('jamburaAutoload');
 #
 # define Paths
-define('JAMBURA_CORE', 'vendor/dodul/jambura-core');
+define('JAMBURA_CORE', 'vendor/prepmock/jambura-core/src/helpers');
 define('JAMBURA_MODS', 'applications/models/');
 define('JAMBURA_CONTROLLERS', 'applications/controllers/');
 define('JAMBURA_VIEWS', 'applications/views/');
